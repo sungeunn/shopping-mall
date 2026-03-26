b386001c16e8f2777c7c73aa0cc756466c29a696
sungeunn <saeun0512@gmail.com>
2026-03-26 19:42:51 +0900

    refactor: @EnableJpaAuditing을 별도 설정 클래스로 분리

--- src/main/java/com/shoppingmall/ShoppingMallApplication.java
+++ src/main/java/com/shoppingmall/ShoppingMallApplication.java
@@ -2,10 +2,7 @@ package com.shoppingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 @SpringBootApplication
-@EnableJpaAuditing
 public class ShoppingMallApplication {
 
     public static void main(String[] args) {
